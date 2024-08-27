b9d38f58fcdb71c2ef6ce4926fe69a2926cf821c
大脇和也 <bp22057@shibaura-it.ac.jp>
2024-08-27 11:33:32 +0900

    ファイルサイズの表示補正の追加

--- detailscript.js
+++ detailscript.js
@@ -31,6 +31,18 @@ let db;
             request.onsuccess = function(event) {
                 const fileData = event.target.result;
                 if (fileData) {
+                     let fileSizeCr; // 変数をここで宣言
+            
+                // ファイルサイズをMBに変換
+                if (fileData.fileSize >= 1024 && fileData.fileSize < 1024 * 1024) {
+                    fileSizeCr = (fileData.fileSize / 1024).toFixed(2) + ' KB'; // KBに変換
+                } else if (fileData.fileSize >= 1024 * 1024 && fileData.fileSize < 1024 * 1024 * 1024) {
+                    fileSizeCr = (fileData.fileSize / (1024 * 1024)).toFixed(2) + ' MB'; // MBに変換
+                } else if (fileData.fileSize >= 1024 * 1024 * 1024) {
+                    fileSizeCr = (fileData.fileSize / (1024 * 1024 * 1024)).toFixed(2) + ' GB'; // GBに変換
+                } else {
+                    fileSizeCr = fileData.fileSize + ' B'; // バイトで表示
+                }
                     const detailsDiv = document.getElementById('fileDetails');
                     detailsDiv.innerHTML = `
                         <p>ファイル名: ${fileData.fileName}</p>
@@ -38,7 +50,7 @@ let db;
                         <p>アップロード日時: ${fileData.uploadDate}</p>
                         <p>パスワードの有無: ${fileData.hasPassword ? 'あり' : 'なし'}</p>
                         <p>ファイル形式: ${fileData.fileExtension}</p> 
-                        <p>ファイルサイズ: ${fileData.fileSize} バイト</p> 
+                        <p>ファイルサイズ: ${fileSizeCr}</p> 
                         <p>備考欄: ${fileData.comment}</p> 
                     `;
                 } else {
